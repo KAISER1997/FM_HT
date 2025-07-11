bf063279aa31e13d264ecdd4a502faf7c383133a
KAISER1997 <kingkenshin1997@gmail.com>
2025-07-11 18:50:55 +0200

    before snellius

--- C_NETS/network.py
+++ C_NETS/network.py
@@ -400,39 +400,44 @@ class TTF_layer(nn.Module):
  
 
 class MLP_W_ttflayers(nn.Module):
-    def __init__(self, input_dim: int = 2, time_dim: int = 1, hidden_dim: int = 128,transform='basic'):
+    def __init__(self,base_model, input_dim: int = 2, time_dim: int = 1, hidden_dim: int = 128,transform='basic'):
         super().__init__()
 
         self.input_dim = input_dim
         self.time_dim = time_dim
         self.hidden_dim = hidden_dim
+        self.base_model=base_model
+        self.TTF=TTF_layer(input_dim,transform)
 
-        self.main = nn.Sequential(
-            nn.Linear(input_dim+time_dim, hidden_dim),
-            # TTF_layer(hidden_dim),
-            Swish(),
-            nn.Linear(hidden_dim, hidden_dim),
-            # TTF_layer(hidden_dim),
-            Swish(),
-            nn.Linear(hidden_dim, hidden_dim),
-            # TTF_layer(hidden_dim),
-            Swish(),
-            nn.Linear(hidden_dim, hidden_dim),
-            # TTF_layer(hidden_dim),
-            Swish(),
-            nn.Linear(hidden_dim, input_dim),
-            TTF_layer(input_dim,transform),
-            )
+        # self.main = nn.Sequential(
+        #     nn.Linear(input_dim+time_dim, hidden_dim),
+        #     # TTF_layer(hidden_dim),
+        #     Swish(),
+        #     nn.Linear(hidden_dim, hidden_dim),
+        #     # TTF_layer(hidden_dim),
+        #     Swish(),
+        #     nn.Linear(hidden_dim, hidden_dim),
+        #     # TTF_layer(hidden_dim),
+        #     Swish(),
+        #     nn.Linear(hidden_dim, hidden_dim),
+        #     # TTF_layer(hidden_dim),
+        #     Swish(),
+        #     nn.Linear(hidden_dim, input_dim),
+        #     TTF_layer(input_dim,transform),
+        #     )
+        
 
 
     def forward(self, x: Tensor, t: Tensor) -> Tensor:
         sz = x.size()
-        x = x.reshape(-1, self.input_dim)
-        t = t.reshape(-1, self.time_dim).float()
+        output=self.TTF(self.base_model(x,t))
+        # print(output.shape,"hello")
+        # x = x.reshape(-1, self.input_dim)
+        # t = t.reshape(-1, self.time_dim).float()
 
-        t = t.reshape(-1, 1).expand(x.shape[0], 1)
-        h = torch.cat([x, t], dim=1)
-        output = self.main(h)
+        # t = t.reshape(-1, 1).expand(x.shape[0], 1)
+        # h = torch.cat([x, t], dim=1)
+        # output = self.main(h)
 
-        return output.reshape(*sz)
+        return output
     
